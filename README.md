16852e23351e6e1651db51fcd86d8cfa0d44ccb7
Iain Watt <iainskwatt@gmail.com>
2021-07-05 18:05:13 +0100

    Added skeleton html, css and js to test to make sure all is working. Added README.md too

---
+++ README.md
@@ -0,0 +1,1 @@
+Simple Etch-a-Sketch web browser sketching pad. Learning JavaScript, HTML and CSS via The Odin Project
---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+const container = document.getElementById('draw-space');
+
+const div = document.createElement('div');
+div.classList.add('pixel');
+
+container.appendChild(div);
+
+div.textContent = 'DO I EXIST!!?'
+
+
+
