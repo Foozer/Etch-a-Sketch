dc38c5805d22712eeeab76f4e8d8d95354a8caee
Iain Watt <iainskwatt@gmail.com>
2021-07-06 15:21:37 +0100

    Added a grid and ability to turn on background color to look like a pixel painting program.

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,27 @@
-const container = document.getElementById('draw-space');
-
-const div = document.createElement('div');
-div.classList.add('pixel');
-
-container.appendChild(div);
-
-div.textContent = 'DO I EXIST!!?'
+const gridSizeX = 16;
+const gridSizeY = 16;
+const container = document.getElementById("draw-space");
 
 
+
+function makeRows(rows, cols) {
+  container.style.setProperty('--grid-rows', rows);
+  container.style.setProperty('--grid-cols', cols);
+  for (c = 0; c < (rows * cols); c++) {
+    let cell = document.createElement("div");
+    container.appendChild(cell).className = "grid-item";
+  };
+};
+
+makeRows(gridSizeX, gridSizeY);
+
+const cellItem = document.getElementsByClassName('grid-item');
+console.log(cellItem);
+
+Array.from(cellItem).forEach(element => {
+    element.addEventListener("mouseenter", function( event ) {
+        event.target.style.backgroundColor = 'black';
+    }); 
+    
+});
 
