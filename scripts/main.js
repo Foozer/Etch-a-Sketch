57056e79fe0b099873b92c5ee85e845335b10bac
Iain Watt <iainskwatt@gmail.com>
2021-07-06 16:59:42 +0100

    Created logic ready for users to enter their grid size. This also keeps the drawing area the same just changes the 'pixel' sizes so the grid doesn't change size. 'pixels' just get smaller the more there are.

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,19 @@
-const gridSizeX = 15;
-const gridSizeY = 15;
+const gridSizeX = 50;
+const gridSizeY = 50;
+const drawGridSize = 500;
 const container = document.getElementById("draw-space");
 const cellItem = document.getElementsByClassName('grid-item');
 const clearButton = document.getElementById("clear");
 let color = 'black';
+let squareSize = drawGridSize/gridSizeY;
 
 
 
 function makeRows(rows, cols) {
   container.style.setProperty('--grid-rows', rows);
   container.style.setProperty('--grid-cols', cols);
+  container.style.setProperty('grid-template-rows', `repeat(var(--grid-rows), ${squareSize}px)`);
+  container.style.setProperty('grid-template-columns', `repeat(var(--grid-cols), ${squareSize}px)`);
   for (c = 0; c < (rows * cols); c++) {
     let cell = document.createElement("div");
     container.appendChild(cell).className = "grid-item";
